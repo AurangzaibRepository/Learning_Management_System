bce82dfbf6f79a2eef54708bb73b9e8effe194e1
Aurangzaib Najam <Aurangzaib Najam>
2022-06-02 15:33:02 +0500

    Validates body data

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -2,9 +2,25 @@ const db = require('../models');
 const user = db.user;
 const requestHelper = require('../helpers/request.helper');
 
-exports. register = async(req, res) => {
+exports.register = async(req, res) => {
 
     try {
+        
+        let {
+            first_name,
+            last_name,
+            email,
+            phone_number,
+            role
+        } = req.body;
+
+        data = {first_name, last_name, email, phone_number, role};
+        let validateResponse = requestHelper.validate(data);
+
+        if (!validateResponse.valid) {
+            return requestHelper.response(res, false, validateResponse.messages);
+        }
+
         return requestHelper.response(res, true);
     } catch (exception) {
         return requestHelper.response(res, false, exception.message);
