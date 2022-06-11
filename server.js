7b63f9f7be0fe47ee0c7ced1b2abd6f0305af4a6
Aurangzaib Najam <Aurangzaib Najam>
2022-06-11 16:24:04 +0500

    Specifies multer configuration

--- server.js
+++ server.js
@@ -10,8 +10,20 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(multer().none() );
 
+// Multer configuration
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+var upload = multer({storage: storage});
+app.use(upload.single('profile_picture') );
+
+// Models
 const db = require('./app/models');
 db.sequelize.sync();
 
