a3bdb38987f1bacc1f05bab75e7f6ea53a273e69
Aurangzaib Najam <Aurangzaib Najam>
2022-06-11 14:36:58 +0500

    Creates register validator class

---
+++ app/validators/auth.validator.js
@@ -0,0 +1,11 @@
+const {body} = require('express-validator');
+
+exports.validateRegister = () => {
+    return [
+        body('first_name', 'First name is required ').notEmpty(),
+        body('last_name', 'Last name is required').notEmpty(),
+        body('email', 'Invalid email').isEmail(),
+        body('phone_number', 'Phone number exists').notEmpty(),
+        body('role', 'Invalid role').isIn(['instructor', 'learner'])
+    ];
+}
