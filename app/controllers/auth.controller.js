8bf7ed3d97678a179a6441ab0f333cbe605ba954
Aurangzaib Najam <Aurangzaib Najam>
2022-06-11 14:38:31 +0500

    Applies registe rvalidation

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,26 +1,18 @@
 const db = require('../models');
 const user = db.user;
+const {validationResult} = require('express-validator');
 const requestHelper = require('../helpers/request.helper');
 
 exports.register = async(req, res) => {
 
     try {
-        
-        let {
-            first_name,
-            last_name,
-            email,
-            phone_number,
-            role
-        } = req.body;
-      
-        data = {first_name, last_name, email, phone_number, role};
-        let validateResponse = requestHelper.validate(data);
 
-        if (!validateResponse.valid) {
-            return requestHelper.response(res, false, validateResponse.messages);
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return requestHelper.response(res, false, errors.array());
         }
-
+        
         return requestHelper.response(res, true);
     } catch (exception) {
         return requestHelper.response(res, false, exception.message);
--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../controllers/auth.controller');
+const validator = require('../validators/auth.validator');
 
 module.exports = app => {
 
-    router.post('/register', auth.register);
+    router.post(
+        '/register',
+        validator.validateRegister(),
+        auth.register
+    );
+
     app.use('/auth', router);
 }
