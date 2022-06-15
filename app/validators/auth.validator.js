d595bf0b62459085413e9bbb805f112c3876ac72
Aurangzaib Najam <Aurangzaib Najam>
2022-06-15 18:17:49 +0500

    Specifies message for invaldi email format

--- app/validators/auth.validator.js
+++ app/validators/auth.validator.js
@@ -4,7 +4,7 @@ exports.validateRegister = () => {
     return [
         body('first_name', 'First name is required ').notEmpty(),
         body('last_name', 'Last name is required').notEmpty(),
-        body('email', 'Invalid email').isEmail(),
+        body('email', 'Invalid email').isEmail().withMessage('InvalidEmail'),
         body('phone_number', 'Phone number is required').notEmpty(),
         body('role', 'Invalid role').isIn(['instructor', 'learner'])
     ];
