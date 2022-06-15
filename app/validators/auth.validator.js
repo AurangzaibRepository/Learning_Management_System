07ded26cccdece6c374e1c94f4fe2ffb0fbd55bb
Aurangzaib Najam <Aurangzaib Najam>
2022-06-15 18:26:21 +0500

    Implements validation for duplicaet email

--- app/validators/auth.validator.js
+++ app/validators/auth.validator.js
@@ -1,10 +1,21 @@
 const {body} = require('express-validator');
+const db = require('../models');
+const user = db.user;
 
 exports.validateRegister = () => {
     return [
         body('first_name', 'First name is required ').notEmpty(),
         body('last_name', 'Last name is required').notEmpty(),
-        body('email', 'Invalid email').isEmail().withMessage('InvalidEmail'),
+        body('email').isEmail().withMessage('Invalid Email')
+            .custom(async (email) => {
+                let recordCount = await user.count({
+                    where: {email: email}
+                });
+
+                if (recordCount > 0) {
+                    throw new Error('Email already exists');
+                }
+            }),
         body('phone_number', 'Phone number is required').notEmpty(),
         body('role', 'Invalid role').isIn(['instructor', 'learner'])
     ];
