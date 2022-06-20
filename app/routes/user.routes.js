655f2ec395b974d3ab7cf189cfac30ad5723f80a
Aurangzaib Najam <Aurangzaib Najam>
2022-06-20 15:26:10 +0500

    Call user controller function in get route

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const user = require('../controllers/user.controller');
 
 module.exports = app => {
 
-    router.get('/:id', (req, res) => {
-        console.log(req.params.id);
-    });
+    router.get('/:id', user.get);
 
     app.use('/user', router);
 }
