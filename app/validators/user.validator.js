060518d9c4583d72b40df4182924a7b94c2d7651
Aurangzaib Najam <Aurangzaib Najam>
2022-06-20 15:48:37 +0500

    Creates user validator to check user exists

--- app/validators/user.validator.js
+++ app/validators/user.validator.js
@@ -1,12 +1,11 @@
 const {check} = require('express-validator');
 const db = require('../models');
-const user = db.user;
 
 exports.validateGet = () => {
     return [
         check('id').custom(async(id) => {
 
-            let data = await user.findByPk(id);
+            let data = await db.user.findByPk(id);
 
             if (!data) {
                 throw new Error('User not found');
