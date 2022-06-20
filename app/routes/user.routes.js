693c3e0b5be2a8f0e5d538e880f9097ff96dcf2d
Aurangzaib Najam <Aurangzaib Najam>
2022-06-20 15:42:20 +0500

    Assings validator to get route

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const user = require('../controllers/user.controller');
+const validator = require('../validators/user.validator');
 
 module.exports = app => {
 
-    router.get('/:id', user.get);
+    router.get(
+        '/:id', 
+        validator.validateGet(),
+        user.get
+    );
 
     app.use('/user', router);
 }
