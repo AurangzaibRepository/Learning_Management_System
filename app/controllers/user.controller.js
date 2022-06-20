361aba831ef5b812fdaa0fc0fbe94fcec7ae1dc3
Aurangzaib Najam <Aurangzaib Najam>
2022-06-20 15:42:47 +0500

    Returns validation error if exists

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,12 +1,18 @@
 var db = require('../models');
+const {validationResult} = require('express-validator');
 var requestHelper = require('../helpers/request.helper');
 
 exports.get = async(req, res) => {
 
     try {
+        const errors = validationResult(req).formatWith(({msg}) => msg);
+        
+        if (!errors.isEmpty()) {
+            return requestHelper.response(res, false, errors.array());
+        }
 
         return requestHelper.response(res, true, null, null);
     } catch(exception) {
         return requestHelper.response(res, false, exception.message);
     }
-}
+} 
