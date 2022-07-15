efd41c1fe12037fb0c1b6440376f67160742373e
Aurangzaib Najam <Aurangzaib Najam>
2022-07-16 01:42:25 +0500

    Resolves eslitning issues on model index

--- app/models/index.js
+++ app/models/index.js
@@ -2,15 +2,15 @@ const config = require('../config/db.config');
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
-    host: config.HOST,
-    dialect: config.dialect,
-    operatorsAliases: false, 
-    pool: {
-        min: config.pool.min,
-        max: config.pool.max,
-        acquire: config.pool.acquire,
-        idle: config.pool.idle
-    }
+  host: config.HOST,
+  dialect: config.dialect,
+  operatorsAliases: false,
+  pool: {
+    min: config.pool.min,
+    max: config.pool.max,
+    acquire: config.pool.acquire,
+    idle: config.pool.idle,
+  },
 });
 
 const db = {};
