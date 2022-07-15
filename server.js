609b9d4207a2327a1353cc836dfa2d8e3b01e493
Aurangzaib Najam <Aurangzaib Najam>
2022-07-16 01:49:04 +0500

    Resolves linting issues

--- server.js
+++ server.js
@@ -2,26 +2,26 @@ const express = require('express');
 const multer = require('multer');
 const cors = require('cors');
 const dotenv = require('dotenv');
-var app = express();
+const app = express();
 
-var corsOptions = {
-    origin: "http://127.0.0.1:3000"
-  };
+const corsOptions = {
+  origin: 'http://127.0.0.1:3000',
+};
 app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 // Multer configuration
-var storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'uploads');
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'uploads');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
-var upload = multer({storage: storage});
+const upload = multer({storage: storage});
 app.use(upload.single('profile_picture') );
 
 // Models
@@ -31,10 +31,9 @@ db.sequelize.sync();
 // Routes
 require('./app/routes/auth.routes')(app);
 
-
 dotenv.config();
 
-const port = process.env.PORT || 86
+const port = process.env.PORT || 86;
 app.listen(port, () => {
-    console.log(`Server is running on ${port}`);
+  console.log(`Server is running on ${port}`);
 });
