f601152464c068518b0be314827c3196489d6083
Aurangzaib Najam <Aurangzaib Najam>
2022-07-19 16:46:17 +0500

    Registers static middlware

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static('uploads'));
 
 // Multer configuration
 const storage = multer.diskStorage({
