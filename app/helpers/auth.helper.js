7f3c6c13be9ead54364202a32033d3fec6e621c0
Aurangzaib Najam <Aurangzaib Najam>
2022-07-19 14:33:47 +0500

    Applies eslint changes

--- app/helpers/auth.helper.js
+++ app/helpers/auth.helper.js
@@ -1,12 +1,10 @@
 const jwt = require('jsonwebtoken');
-const config = require('../../config.json');
 
 exports.getJWT = (userID) => {
+  const tokenData = {
+    time: Date(),
+    user_id: userID,
+  };
 
-    let tokenData = {
-        time: Date(),
-        user_id: userID
-    };
-
-    return jwt.sign(tokenData, process.env.JWT_SECRET_KEY);
-}
+  return jwt.sign(tokenData, process.env.JWT_SECRET_KEY);
+};
