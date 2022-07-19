a940c01be8a06bb7d33afd380966b3f1f00fe401
Aurangzaib Najam <Aurangzaib Najam>
2022-07-19 19:27:51 +0500

    Resolves eslint issues in get

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -2,22 +2,25 @@ var db = require('../models');
 const {validationResult} = require('express-validator');
 var requestHelper = require('../helpers/request.helper');
 
-exports.get = async(req, res) => {
+exports.get = async (req, res) => {
+  try {
+    const errors = validationResult(req).formatWith(({msg}) => msg);
 
-    try {
-        const errors = validationResult(req).formatWith(({msg}) => msg);
-        
-        if (!errors.isEmpty()) {
-            return requestHelper.response(res, false, errors.array());
-        }
-
-        let user = await db.user.findByPk(req.params.id);
-        return requestHelper.response(res, true, null, db.user.generateProfile(user));
-
-    } catch(exception) {
-        return requestHelper.response(res, false, exception.message);
+    if (!errors.isEmpty()) {
+      return requestHelper.response(res, false, errors.array());
     }
-} 
+
+    const user = await db.user.findByPk(req.params.id);
+    return requestHelper.response(
+        res,
+        true,
+        null,
+        db.user.generateProfile(user),
+    );
+  } catch (exception) {
+    return requestHelper.response(res, false, exception.message);
+  }
+};
 
 exports.updateProfile = async(req, res) => {
 
