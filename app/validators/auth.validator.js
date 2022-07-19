9183e182fb951e0f68752cb8d927a5df84c77b99
Aurangzaib Najam <Aurangzaib Najam>
2022-07-19 15:54:31 +0500

    Applies eslint changes to auth validator

--- app/validators/auth.validator.js
+++ app/validators/auth.validator.js
@@ -4,43 +4,47 @@ const db = require('../models');
 const user = db.user;
 
 exports.validateRegister = () => {
-    return [
-        body('first_name', 'First name required ').notEmpty(),
-        body('last_name', 'Last name required').notEmpty(),
-        body('email').isEmail().withMessage('Invalid Email')
-            .custom(async (email) => {
-                let recordCount = await user.count({
-                    where: {email: email}
-                });
+  return [
+    body('first_name', 'First name required ').notEmpty(),
+    body('last_name', 'Last name required').notEmpty(),
+    body('email').isEmail().withMessage('Invalid Email')
+        .custom(async (email) => {
+          const recordCount = await user.count({
+            where: {email: email},
+          });
 
-                if (recordCount > 0) {
-                    throw new Error('Email already exists');
-                }
-            }),
-        body('password', 'Password required').notEmpty(),
-        body('phone_number', 'Phone number required').notEmpty(),
-        body('role', 'Invalid role').isIn(['instructor', 'learner'])
-    ];
-}
+          if (recordCount > 0) {
+            throw new Error('Email already exists');
+          }
+        }),
+    body('password', 'Password required').notEmpty(),
+    body('phone_number', 'Phone number required').notEmpty(),
+    body('role', 'Invalid role').isIn(['instructor', 'learner']),
+  ];
+};
 
 exports.validateLogin = () => {
-    return [
-        body('email').notEmpty().withMessage('Email required')
-            .isEmail().withMessage('Invalid email')
-            .custom(async(email, {req}) => {
-                const userProfile = await user.findOne({
-                    where: {email: email}
-                });
+  return [
+    body('email').notEmpty().withMessage('Email required')
+        .isEmail().withMessage('Invalid email')
+        .custom(async(email, {req}) => {
+          const userProfile = await user.findOne({
+            where: {email: email},
+          });
 
-                if (!userProfile) {
-                    throw new Error('User does not exist');
-                }
+          if (!userProfile) {
+            throw new Error('User does not exist');
+          }
 
-                const validPassword = await bcrypt.compare(req.body.password, userProfile.password);
-                if (!validPassword) {
-                    throw new Error('User does not exist');
-                }
-            }),
-        body('password', 'Password required').notEmpty()
-    ];
-}
+          const validPassword = await bcrypt.compare(
+              req.body.password,
+              userProfile.password,
+          );
+
+          if (!validPassword) {
+            throw new Error('User does not exist');
+          }
+        }),
+    body('password', 'Password required').notEmpty(),
+  ];
+};
