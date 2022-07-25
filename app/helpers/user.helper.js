97ce224f1294d421afb43c4d031ed5bd41c82805
Aurangzaib Najam <Aurangzaib Najam>
2022-07-25 16:41:00 +0500

    Specifies profile picture path

--- app/helpers/user.helper.js
+++ app/helpers/user.helper.js
@@ -1,11 +1,18 @@
 const authHelper = require('./auth.helper');
 
 exports.generateProfile = (user) => {
-  return {
+  const data = {
     user_id: user.id,
     first_name: user.first_name,
     last_name: user.last_name,
-    profile_picture: `/profile/${user.profile_picture}`,
     token: authHelper.getJWT(user.id),
   };
+
+  if (user.profile_picture) {
+    data.profile_profile = `/profile/${user.profile_picture}`;
+  } else {
+    data.profile_picture = '/profile/male-prof.jpeg';
+  }
+
+  return data;
 };
